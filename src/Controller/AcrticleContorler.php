9694341f42a914bd1581bb822a9e8e60a7582661
NikitaKechovski <nikita@embedsocial.com>
2021-05-21 13:48:23 +0200

    Progress

--- src/Controller/AcrticleContorler.php
+++ src/Controller/AcrticleContorler.php
@@ -5,24 +5,33 @@ namespace App\Controller;
 
 
 use http\Env\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AcrticleContorler
+class AcrticleContorler extends AbstractController
 {
     /**
      * @Route("/")
-     */
+
     public function homepage()
     {
         return new Response('My first webpage!');
     }
 
-    /**
      * @Route("/news/{slug}")
      */
 
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show the article: %s'));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
 
     }
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -35,12 +35,18 @@ class Kernel extends BaseKernel
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
         $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
         $container->setParameter('container.dumper.inline_factories', true);
+        /*$container->loadFromExtension('twig', [
+            'debug' => '%kernel.debug%',
+            // ...
+        ]);*/
+
         $confDir = $this->getProjectDir().'/config';
 
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes): void
